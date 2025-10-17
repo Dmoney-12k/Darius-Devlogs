a2f7369fde662d64ffe46fbc17af6e6e651ad1f7
Darius <dmontgomery@icstars.org>
2025-10-17 18:06:41 -0500

    Dmoney12k Devlog

---
+++ script.js
@@ -0,0 +1,31 @@
+let numbers = "6";
+console.log(numbers);
+let number = 6;
+let x = number + numbers;
+console.log(x);
+let one = 1;
+let z = numbers+ one;
+console.log(z)
+
+let oneword ="Darius";
+let twoword ="Montgomery";
+let cto = oneword +""+ twoword;
+console.log(cto)
+
+function add (a,b){
+let c = a + b
+console.log(c)
+}
+add(33,22);
+
+let y = "5";
+console.log(y===5);
+let word = false;
+let anotherWord = false;
+console.log(word ===anotherWord);
+
+function add(a,b,c){
+let sum = a + b+c;
+console.log(sum);
+}
+add(1,2,3);
